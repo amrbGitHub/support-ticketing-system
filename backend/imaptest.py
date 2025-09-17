83c705aac09e50dd7ad7d9874897d7faa394d0f9
Amro_Belbeisi <amrbelbeisi002@gmail.com>
2025-09-17 17:00:12 +0300

    Successfully integrated IMAP with DB, will merge this branch

--- backend/imaptest.py
+++ backend/imaptest.py
@@ -9,7 +9,7 @@ from imap_tools import MailBox, AND
 from emails.models import EmailMessages
 
 # For exists message
-BOLD = '\003[1m'
+
 RED = '\033[91m'
 END = '\033[0m'
 
@@ -28,13 +28,13 @@ def fetch_email_info():
         latest_email = sorted_emails[0] # get the first element of the list (latest email that was sent to us)
         # Write the email in a text file
         file_name = f"email_{latest_email.uid}.txt" 
-        filepath = os.path.join("D:/Desktop/nexjts-ticketing-system/recievedmails", file_name)
+        filepath = os.path.join("recievedmails", file_name)
         content_of_file = latest_email.date, latest_email.from_, latest_email.subject, latest_email.text # some data for the email
         # TODO: Put email information into database table instead just writing it into file
         
         #if the object already exists, do not create a new one, else do create a new one
         if EmailMessages.objects.filter(mail_from=latest_email.from_).exists():
-            print(BOLD + RED + "Object already exists in Database" + END)
+            print(RED + "Object already exists in Database" + END)
             didfetch = False
         else:
             EmailMessages.objects.create(
