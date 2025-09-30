7fcebe065e19777b80ad9fa1bd3d16e66cd3e803
amrbGitHub <amrbelbeisi002@gmail.com>
2025-09-30 13:01:59 +0300

    Directory cleanup

--- backend/emails/urls.py
+++ backend/emails/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 from .views import ReplyView
 urlpatterns = [
-    #path ("", views.index, name="index"),
-    #path("<int:email>/", views.detail, name="detail"),
     path("reply/<int:pk>/", ReplyView.as_view(), name="reply"),
-    # path("complete/<int:pk>/", mark_complete, name="mark-complete"),
 ]
--- backend/smtptest.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP('smtp.gmail.com', 587) # start smtp server will TLS port
-server.starttls() # start server with TLS 
-# Login to gmail account
-# password : apln uumy etdw bodj 
-server.login('amrstestemail4dev@gmail.com', 'apln uumy etdw bodj')
