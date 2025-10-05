8fb65d3fbf6aefc03aee4e7d22f32d2450946be2
Amro_Belbeisi <amrbelbeisi002@gmail.com>
2025-10-05 23:27:34 +0300

    fixing reply issue

--- frontend/app/emails/[id]/ReplyBox.jsx
+++ frontend/app/emails/[id]/ReplyBox.jsx
@@ -13,7 +13,7 @@ export default function ReplyBox({ ticketId }) {
 
     try {
       const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000"
-      const res = await fetch(`${API_URL}/api/reply/${ticketId}/`, {
+      const res = await fetch(`${API_URL}/reply/${ticketId}/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
