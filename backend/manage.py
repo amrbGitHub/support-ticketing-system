9eca72006a7c0f32e8b250610dc2f5467ada79d0
amrbGitHub <amrbelbeisi002@gmail.com>
2025-10-05 12:15:51 +0300

    For render configs

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myTicketingSite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ticketingsite.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- backend/ticketingsite/urls.py
+++ backend/ticketingsite/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from myTicketingSite.routers import router
+from ticketingsite.routers import router
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include((router.urls))),
