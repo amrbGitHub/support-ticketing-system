f41357f51281bd3f35e34843d7fd98387b861a86
amrbGitHub <amrbelbeisi002@gmail.com>
2025-10-05 20:25:00 +0300

    .

---
+++ frontend/app/utils/api.ts
@@ -0,0 +1,13 @@
+
+export async function FetchTickets(){
+    const API_URL= process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+    const res = await fetch(`${API_URL}/api/emails`, {
+        cache: "no-store",
+});
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch tickets: ${res.status} ${res.statusText}`);
+  }
+
+return res.json();
+}
